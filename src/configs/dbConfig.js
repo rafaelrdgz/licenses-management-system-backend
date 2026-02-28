e4efe5b5a44552376baeb8785bb114c664d93d25
Rafael Rodriguez <rafaelrdgz2003@gamil.com>
2026-02-28 14:59:11 -0500

    Require SSL for database connections in dbConfig

--- src/configs/dbConfig.js
+++ src/configs/dbConfig.js
@@ -11,6 +11,9 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+  ssl: {
+    require: true,
+  },
 });
 
 export default pool;
